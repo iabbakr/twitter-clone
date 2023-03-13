4c1dc8ffe5b7efd82bf9df6fc7303f608fafb96a
iabbakr <abubakaribrahim603@gmail.com>
2023-03-13 09:46:26 +0100

    added fix to js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import {tweetsData} from './data.js';
+
+const tweetInput = document.getElementById('tweet-input');
+const tweetBtn = document.getElementById('tweet-btn');
+const feedDisplay = document.getElementById('feed');
+
+tweetBtn.addEventListener('click', function(){
+    feed.textContent = tweetInput.value;
+    tweetInput.value = '';
+
+
+    
+});
