5113aa75a97c378ca0ffba833436a0ca155b21c7
iabbakr <abubakaribrahim603@gmail.com>
2023-03-13 10:38:44 +0100

    added fix to js

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ import {tweetsData} from './data.js';
 
 const tweetInput = document.getElementById('tweet-input');
 const tweetBtn = document.getElementById('tweet-btn');
-const feedDisplay = document.getElementById('feed');
+const feedD = document.getElementById('feed');
 
 tweetBtn.addEventListener('click', function(){
-    feedDisplay.textContent = tweetInput.value;
-    tweetInput.value = '';
-
-
-    
+    getFeedHtml();  
 });
+
+function getFeedHtml(){
+    let feedHtml = ''
+
+    for (let tweet of tweetsData){
+        feedHtml += `
+                        <div class="tweet">
+                            <div class="tweet-inner">
+                                <img src="${tweet.profilePic}" class="profile-pic">
+                                <div>
+                                    <p class="handle">${tweet.handle}</p>
+                                    <p class="tweet-text">${tweet.tweetText}</p>
+                                    <div class="tweet-details">
+                                        <span class="tweet-detail">
+                                        ${tweet.replies}
+                                        </span>
+                                        <span class="tweet-detail">
+                                        ${tweet.likes}
+                                        </span>
+                                        <span class="tweet-detail">
+                                        ${tweet.retweets}
+                                        </span>
+                                    </div>   
+                                </div>            
+                            </div>
+                        </div>
+                    `;
+    };
+    feedD.innerHTML += feedHtml 
+};
